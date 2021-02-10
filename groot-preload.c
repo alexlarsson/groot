a04d5c2d7d6a5bacb19399b12dd45ff776e09905
Alexander Larsson <alexl@redhat.com>
2021-02-10 11:58:10 +0100

    preload: Make unsetenv work in the init
    For some weird reason I can't understand unsetenv() doesn't seem to
    work when called in the init function, so I replaced the libc call it
    with a trivial reimplementation.
    
    You could see this by using something like:
    
    LD_PRELOAD=`pwd`/libgroot.so /bin/sh -c 'echo a | cat | cat '
    Which will print:
      groot: Warning: no defined ids for user root in /etc/subuid, limited user/group support
      groot: Warning: no defined ids for user root in /etc/subgid, limited user/group support
      groot: Warning: no defined ids for user root in /etc/subuid, limited user/group support
      groot: Warning: no defined ids for user root in /etc/subgid, limited user/group support
    a
    
    Because the echo and the cat processes also try to do the LD_PRELOAD.
    
    We also set a GROOT_DISABLED flag to combat reetrancy because in yocto sometimes the LD_PRELOAD
    is re-added to ten environment, even though we unset it.
    
    foo

--- groot-preload.c
+++ groot-preload.c
@@ -4,14 +4,49 @@
 #include "utils.h"
 #include "groot-ns.h"
 
+/* For some reason the regular unsetenv doesn't seem to work well in an initializer... */
+static void
+__unsetenv (const char *name)
+{
+  size_t len = strlen (name);
+  char **ep;
+
+  ep = __environ;
+  while (*ep != NULL)
+    {
+      if (strncmp (*ep, name, len) == 0 && (*ep)[len] == '=')
+        {
+          char **dp = ep;
+          do
+            dp[0] = dp[1];
+          while (*dp++);
+        }
+      else
+        ++ep;
+    }
+}
+
 static void
 _groot_init_main (int argc, char *argv[])
 {
   const char *env_wrap = NULL;
+  const char *disabled = NULL;
   char **wrapdirs = NULL;
   int num_wrapdirs = 0;
 
+  disabled = getenv ("GROOT_DISABLED");
   env_wrap = getenv ("GROOT_WRAPFS");
+
+  /* Don't recursively enable groot */
+  __unsetenv ("LD_PRELOAD");
+
+  if (disabled != NULL)
+    return;
+
+  /* If something re-enables LD_PRELOAD inside, disable it anyway */
+  if (disabled == NULL)
+    setenv ("GROOT_DISABLED", "1", 1);
+
   if (env_wrap)
     {
       autofree char *data = xstrdup (env_wrap);
@@ -26,8 +61,6 @@ _groot_init_main (int argc, char *argv[])
         }
     }
 
-  /* Don't recursively enable groot */
-  unsetenv ("LD_PRELOAD");
 
   groot_setup_ns ((const char **)wrapdirs, num_wrapdirs);
   strfreev (wrapdirs);
