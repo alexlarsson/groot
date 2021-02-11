e52f075a1249eae184606808e5825feb5118e940
Alexander Larsson <alexl@redhat.com>
2021-02-11 12:15:58 +0100

    Remove GROOT_ENABLE env var for now
    We currently don't need this, so why have it.

--- groot-preload.c
+++ groot-preload.c
@@ -7,14 +7,9 @@
 static void
 _groot_init_main (int argc, char *argv[])
 {
-  const char *enable;
   const char *env_wrap = NULL;
   char **wrapdirs = NULL;
   int num_wrapdirs = 0;
-
-  enable = getenv ("GROOT_ENABLE");
-  if (enable == NULL || *enable == 0)
-    return;
 
   env_wrap = getenv ("GROOT_WRAPFS");
   if (env_wrap)
@@ -33,7 +28,6 @@ _groot_init_main (int argc, char *argv[])
 
   /* Don't recursively enable groot */
   unsetenv ("LD_PRELOAD");
-  unsetenv ("GROOT_ENABLE");
 
   groot_setup_ns ((const char **)wrapdirs, num_wrapdirs);
   strfreev (wrapdirs);
